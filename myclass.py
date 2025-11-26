d187ea1de9caff15e63297ac5fd2ce7a84bfa82b
leedongkyu <leedongkyu0225@gmail.com>
2025-11-26 20:55:03 +0900

    feat: python operator overloading

---
+++ myclass.py
@@ -0,0 +1,12 @@
+class Animal:
+    def speak(self):
+        print("소리를 냅니다")
+    def run(self):
+        print("뛰어갑니다")
+class Cat(Animal):
+    def speak(self):
+        print("야옹")        
+cat = Cat()
+cat.speak()
+cat.run()
+        
