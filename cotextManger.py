bf7840ffde0b91f2f14b4135637289cc8f587134
leedongkyu <leedongkyu0225@gmail.com>
2025-11-29 10:47:07 +0900

    feat: overloading and contextmanger

---
+++ cotextManger.py
@@ -0,0 +1,10 @@
+class Light:
+    def __enter__(self): #들어가는 부분
+        print("불을 켭니다")
+        return self
+    def __exit__(self,exc_type,exc_val,exc_tb):
+        print("불을 끕니다.")
+with Light():
+    print("방에서 책을 읽습니다")
+print("with 블럭 밖으로 나왔습니다.")
+        
--- overloading.py
+++ overloading.py
@@ -11,3 +11,4 @@ for i in range(len(squares)):
     print(squares[i],end=" ")
     
         
+        
